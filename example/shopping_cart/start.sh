1f4bf5b79c6958483756fb176635cb9f803683f4
Marcel Lanz <marcellanz@n-1.ch>
2020-04-30 23:33:48 +0200

    [fix] make the example build and run by invoking start.sh and removed absolute path for the script changing to, and, having bash invoked portable. (#2)

--- example/shopping_cart/start.sh
+++ example/shopping_cart/start.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-cd /home/sleipnir/development/workspace/pessoal/cloudstate-repos/cloudstate-dart-support/example/shopping_cart
+pub get
 dart --snapshot-kind=kernel --snapshot=bin/main.dart.snapshot bin/main.dart
 dart bin/main.dart.snapshot
